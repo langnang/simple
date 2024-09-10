5adad2de0983d04dc61f2e149aab36b4bbdc9881
langnang <langnang.chen@outlook.com>
2024-09-10 11:00:59 +0800

    2024-09-10 11:00:57

--- projects/WebSpider/composer/langnang-modules/WebSpider/Routes/web.php
+++ projects/WebSpider/composer/langnang-modules/WebSpider/Routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
-use Illuminate\Router\Facades\Router;
 use Modules\WebSpider\Http\Controllers\WebSpiderController;
 
-Router::prefix(config('this.alias'))->group(function () {
-  Router::get('', [WebSpiderController::class, 'index']);
-  Router::get('content', '\Modules\WebSpider\Http\Controllers\WebSpiderController@content');
-  Router::get('ob-spider', '\Modules\WebSpider\Http\Controllers\WebSpiderController@ob_spider');
+Route::prefix(config('this.alias'))->group(function () {
+  Route::get('', [WebSpiderController::class, 'index']);
+  Route::get('content', '\Modules\WebSpider\Http\Controllers\WebSpiderController@content');
+  Route::get('ob-spider', '\Modules\WebSpider\Http\Controllers\WebSpiderController@ob_spider');
 });
